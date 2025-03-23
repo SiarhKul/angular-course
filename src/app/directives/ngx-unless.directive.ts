8b5465815e168d424061e8b74fe3f4c1120d8987
siarhkul <siarhkul@gmail.com>
2025-03-23 15:53:36 +0100

    chore: Directive finish

--- src/app/directives/ngx-unless.directive.ts
+++ src/app/directives/ngx-unless.directive.ts
@@ -7,27 +7,20 @@ export class NgxUnlessDirective {
 
   visible = false;
 
-
   constructor(private templateRef: TemplateRef<any>,
               private viewContainer: ViewContainerRef) {
-
-
   }
 
   @Input()
-  set ngxUnless(condition:boolean) {
+  set ngxUnless(condition: boolean) {
       if (!condition && !this.visible) {
           this.viewContainer.createEmbeddedView(this.templateRef);
           this.visible = true;
-      }
-      else if (condition && this.visible) {
+      } else if (condition && this.visible) {
           this.viewContainer.clear();
           this.visible = false;
       }
-
   }
-
-
 }
 
 
